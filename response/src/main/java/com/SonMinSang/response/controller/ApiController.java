10f059bb46e893052b6ab9c59cc5fe9e48cc1b25
82102 <sms7624@gmail.com>
2022-01-29 23:24:46 +0900

    response

---
+++ response/src/main/java/com/SonMinSang/response/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.SonMinSang.Response.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/text")
+    public String text(@RequestParam String account){
+        return account;
+    }
+}
