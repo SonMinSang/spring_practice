8b84f6eccc34e84343b973427d507ca0802a5993
82102 <sms7624@gmail.com>
2022-01-30 22:34:03 +0900

    response

--- response/src/main/java/com/SonMinSang/response/controller/ApiController.java
+++ response/src/main/java/com/SonMinSang/response/controller/ApiController.java
@@ -1,16 +1,29 @@
-package com.SonMinSang.Response.controller;
+package com.SonMinSang.response.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.SonMinSang.response.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
     @GetMapping("/text")
     public String text(@RequestParam String account){
+
         return account;
+    }
+
+    //JSON
+    // req -> object mapper -> object -> method -> object -> object mapper -> json -> response
+    @PostMapping("/json")
+    public User json(@RequestBody User user){
+        return user;
+    }
+
+    //Response
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
