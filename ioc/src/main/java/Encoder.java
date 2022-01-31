eb1687ebbef374de99912625bae1ba31d29e03ab
82102 <sms7624@gmail.com>
2022-01-31 23:34:25 +0900

    DI

---
+++ ioc/src/main/java/Encoder.java
@@ -0,0 +1,11 @@
+import java.util.Base64;
+
+public class Encode {
+
+    public class Encoder {
+
+        public String encode(String message){
+            return Base64.getEncoder().encodeToString(message.getBytes());
+        }
+    }
+}
