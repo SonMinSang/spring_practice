ca38e80e41eb997c575a80569d3d4c4b757eaf7a
82102 <sms7624@gmail.com>
2022-02-07 23:36:38 +0900

    database access

--- inflearn/hello-spring/src/main/java/son/hellospring/SpringConfig.java
+++ inflearn/hello-spring/src/main/java/son/hellospring/SpringConfig.java
@@ -1,21 +1,35 @@
 package son.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import son.hellospring.repository.MemberRepository;
-import son.hellospring.repository.MemoryMemberRepository;
+import son.hellospring.repository.*;
 import son.hellospring.service.MemberService;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    //private EntityManager em;
+    //private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new MemberService(memberRepository);
     }
+    //  @Bean
+//    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+
+    //}
 }
--- inflearn/hello-spring/src/main/java/son/hellospring/service/MemberService.java
+++ inflearn/hello-spring/src/main/java/son/hellospring/service/MemberService.java
@@ -2,6 +2,7 @@ package son.hellospring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import son.hellospring.domain.Member;
 import son.hellospring.repository.MemberRepository;
 import son.hellospring.repository.MemoryMemberRepository;
@@ -9,6 +10,7 @@ import son.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
