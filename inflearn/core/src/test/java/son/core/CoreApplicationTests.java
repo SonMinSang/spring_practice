a93e6904821b20b64a4e96322b026e219834dd5d
82102 <sms7624@gmail.com>
2022-02-08 22:52:25 +0900

    스프링 핵심원리 - 기본편

---
+++ inflearn/core/src/test/java/son/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package son.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
