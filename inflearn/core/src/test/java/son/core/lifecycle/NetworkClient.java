ba78c23094ddfe492c79dd16d8018ec174092b38
82102 <sms7624@gmail.com>
2022-02-18 00:42:06 +0900

    PostConstruct PreDestroy

--- inflearn/core/src/test/java/son/core/lifecycle/NetworkClient.java
+++ inflearn/core/src/test/java/son/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package son.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
     
     private String url;
@@ -25,13 +28,13 @@ public class NetworkClient{
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
-
-
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
