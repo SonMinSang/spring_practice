873df42e6c4748379f5816128ec746a84bbc3e7f
82102 <sms7624@gmail.com>
2022-03-08 23:07:09 +0900

    simple CRUD

--- inflearn/ex1-hello-jpa/src/main/java/jpabasichttps/start/spring/io/ex1hellojpa/hellojpa/JpaMain.java
+++ inflearn/ex1-hello-jpa/src/main/java/jpabasichttps/start/spring/io/ex1hellojpa/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -14,10 +15,32 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            /* 생성
             Member member = new Member();
             member.setId(2L);
             member.setName("HelloB");
-            em.persist(member);
+            em.persist(member);*/
+
+            /* 조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+             */
+
+            /* 수정
+            Member editMember = em.find(Member.class, 1L);
+            editMember.setName("HelloJPA");
+             */
+            /* 삭제
+            Member deleteMember = em.find(Member.class, 1L);
+            em.remove(deleteMember);
+             */
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
